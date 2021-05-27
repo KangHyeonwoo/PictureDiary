7c843c7f3dd3ff5273aebe89a9dba02d89051f82
hwkang <landman93@naver.com>
2021-05-27 23:50:00 +0000

    테스트 코드 작성중

--- src/test/java/com/picture/diary/NasConnectTest.java
+++ src/test/java/com/picture/diary/NasConnectTest.java
@@ -2,12 +2,13 @@ package com.picture.diary;
 
 import com.picture.diary.picture.file.data.Picture;
 import com.picture.diary.picture.file.service.PictureFileService;
-import com.picture.diary.picture.file.service.nas.NasPictureFileService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -41,4 +42,34 @@ public class NasConnectTest {
     }
 
 
+    @Test
+    @DisplayName("NAS 로그인 API API 테스트")
+    void nasLoginApiTest() throws IOException {
+        String apiPath = "https://hwkang.synology.me:5001/webapi/entry.cgi";
+        String userId = "hwkang";
+        String password = "";
+        WebClient webClient = WebClient.builder().build();
+
+        //https://velog.io/@youmakemesmile/%EA%B8%89%ED%95%98%EA%B2%8C-RestTemplate%EB%A5%BC-%EB%8C%80%EC%B2%B4-%ED%95%98%EA%B8%B0-%EC%9C%84%ED%95%9C-Spring-WebFlux-WebClient-%EC%82%AC%EC%9A%A9%EC%84%A4%EB%AA%85%EC%84%9C-WebClient-Bean-%EC%83%9D%EC%84%B1
+        String loginResponse = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(apiPath)
+                        .queryParam("api", "SYNO.API.Auth")
+                        .queryParam("version", "6")
+                        .queryParam("method", "login")
+                        .queryParam("session", "FileStation")
+                        .queryParam("format", "cookie")
+                        .queryParam("account", userId)
+                        .queryParam("password", password)
+                        .build())
+                .retrieve()
+                .onStatus(
+                        httpStatus -> !httpStatus.is2xxSuccessful(),
+                        r -> Mono.empty()
+                )
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(loginResponse);
+    }
 }
