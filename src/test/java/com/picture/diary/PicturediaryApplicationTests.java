2df88c5f41aad5103bc1e1fd4d1ff3384a32b0ea
KangHyeonwoo <landman93@naver.com>
2021-06-11 21:00:48 +0900

    Initial commit

---
+++ src/test/java/com/picture/diary/PicturediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picture.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicturediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
