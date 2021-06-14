955ee4d44e5285419a9b18a3a13097670b691f87
KangHyeonwoo <landman93@naver.com>
2021-06-14 21:48:36 +0900

    파일 경로 프로퍼티 파일 추가 및 테스트

--- src/main/java/com/picture/diary/extract/data/FilePathProperties.java
+++ src/main/java/com/picture/diary/extract/data/FilePathProperties.java
@@ -2,10 +2,8 @@ package com.picture.diary.extract.data;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.stereotype.Component;
 
 /**
  * application.properties > file.path.*
