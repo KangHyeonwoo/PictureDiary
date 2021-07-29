07a5c5e59d29017632c58ed8a3bcf4009a17bb89
landman93@naver.com <landman93@naver.com>
2021-07-29 16:03:24 +0900

    주소 리스트 목록에 표출하는 기능 구현중

--- src/main/resources/static/js/Address.js
+++ src/main/resources/static/js/Address.js
@@ -59,6 +59,62 @@ export default class Address {
 			})
 	}
 	
+	static displayPlaces(places) {
+		
+	}
+	
+	static getItem(index, places) {
+		const el = document.createElement('li');
+		let itemStr = 
+				'<span class="markerbg marker_' + (index+1) + '"></span>' +
+	                '<div class="info">' +
+	                '   <h5>' + places.place_name + '</h5>';
+	
+	    if (places.road_address_name) {
+	        itemStr += '    <span>' + places.road_address_name + '</span>' +
+	                    '   <span class="jibun gray">' +  places.address_name  + '</span>';
+	    } else {
+	        itemStr += '    <span>' +  places.address_name  + '</span>'; 
+	    }
+	                 
+	      itemStr += '  <span class="tel">' + places.phone  + '</span>' +
+	                '</div>';           
+	
+	    el.innerHTML = itemStr;
+	    el.className = 'item';
+
+	    return el;
+	}
+	
+	static displayPagination(pagination) {
+		const paginationEl = document.getElementById('pagination');
+		const fragment = document.createDocumentFragment();
+		let i;
+		
+		while(paginationEl.hasChildNodes()) {
+			paginationEl.removeChild(paginationEl.lastChild);
+		}
+		
+		for(i=1; i<=pagination.last; i++) {
+			const el = document.createElement('a');
+			el.href = '#';
+			el.innerHTML = i;
+			
+			if(i === pagination.current) {
+				el.className = 'on';
+			} else {
+				el.onclick = (function(i){
+					return function() {
+						pagination.gotoPage(i);
+					}
+				})(i);
+			}
+			
+			fragment.appendChild(el);
+		}
+		
+		paginationEl.appendChild(fragment);
+	}
 	
 }
 
