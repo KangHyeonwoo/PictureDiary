b8e064ac6c6774d0c4ad8bc835dbe42d404b181c
hwkang <landman93@naver.com>
2022-05-13 00:51:26 +0900

    구현 방식 변경  - HttpConnection 유틸 클래스 구현 후 API 요청 -> WebClient Library 사용

--- src/main/java/com/picture/diary/common/connection/ResponseCode.java
+++ src/main/java/com/picture/diary/common/connection/ResponseCode.java
@@ -1,5 +1,12 @@
 package com.picture.diary.common.connection;
 
 public enum ResponseCode {
-    SUCCESS, FAIL, ERROR
+    //응답 성공. 정상적으로 데이터가 리턴된 경우
+    SUCCESS,
+
+    //응답은 성공하였으나 잘못된 요청 혹은, 필수 파라미터 누락으로 인해 응답 결과가 실패인 경우
+    FAIL,
+
+    //요청 자체를 실패한 경우
+    ERROR;
 }
