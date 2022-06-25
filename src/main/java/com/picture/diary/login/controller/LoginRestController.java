d12031dd9e6c2c2cc64f3faaa94e4cfe77791b08
hwkang <landman93@naver.com>
2022-06-25 22:55:00 +0900

    Validation 추가

--- src/main/java/com/picture/diary/login/controller/LoginRestController.java
+++ src/main/java/com/picture/diary/login/controller/LoginRestController.java
@@ -1,10 +1,13 @@
 package com.picture.diary.login.controller;
 
 import com.picture.diary.common.response.BasicResponse;
+import com.picture.diary.common.response.SuccessResponse;
 import com.picture.diary.login.data.LoginRequestDto;
 import com.picture.diary.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -17,14 +20,17 @@ public class LoginRestController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public BasicResponse login(@Valid LoginRequestDto loginRequestDto) throws IOException {
+    public ResponseEntity<BasicResponse> login(@Valid LoginRequestDto loginRequestDto) throws IOException {
+        BasicResponse response = loginService.login(loginRequestDto);
 
-        return loginService.login(loginRequestDto);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
-    public BasicResponse logout() {
+    public ResponseEntity<BasicResponse> logout(@RequestParam String userId) {
 
-        return null;
+        loginService.logout(userId);
+
+        return ResponseEntity.ok(new SuccessResponse<>());
     }
 }
--- src/main/java/com/picture/diary/login/data/LoginRequestDto.java
+++ src/main/java/com/picture/diary/login/data/LoginRequestDto.java
@@ -1,12 +1,25 @@
 package com.picture.diary.login.data;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
-@AllArgsConstructor
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 public class LoginRequestDto {
 
+    @NotEmpty(message = "ID를 입력해주세요.")
+    @Size(min = 4, max = 20, message = "사용자 ID는 4 ~ 20 글자 사이로 입력이 가능합니다.")
     private String userId;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, max = 20, message = "사용자 비밀번호는 8 ~ 20 글자 사이로 입력이 가능합니다.")
     private String password;
+
+    @Builder
+    public LoginRequestDto(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
 }
