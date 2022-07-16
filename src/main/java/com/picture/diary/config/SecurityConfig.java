4bd690bd3d820756cb2f2809e20a88e7240c71c3
hwkang <landman93@naver.com>
2022-07-16 22:31:29 +0900

    Spring Security 적용하기 위한 고민중

--- src/main/java/com/picture/diary/config/SecurityConfig.java
+++ src/main/java/com/picture/diary/config/SecurityConfig.java
@@ -3,44 +3,18 @@ package com.picture.diary.config;
 import com.picture.diary.login.data.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+/**
+ * @Reference : https://devuna.tistory.com/59
+ */
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        //static 디렉토리 하위 파일 목록은 인증 무시( = 항상 통과)
-        web.ignoring().antMatchers(
-                "/css/**", "/js/**", "/img/**", "/lib/**", "/less/**", "/assets/**"
-        );
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable();
-
-        http.authorizeRequests()
-                .antMatchers("/")
-                .permitAll();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-
-    }
-    */
+    private AuthenticationProvider authenticationProvider;
 
     private final static String[] PERMIT_ALL_PATTERNS = {"/", "/css/**" , "/images/**", "/js/**"};
     private final static String[] USER_ROLE_PATTERNS = {"/map"};
