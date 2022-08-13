9df01e1500abf2a8bf358af6cf3f664cca51dc1f
hwkang <landman93@naver.com>
2022-08-13 13:01:51 +0000

    JWT access-token & refresh-token 으로 나누는 중

--- src/main/java/com/picture/diary/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/picture/diary/common/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.picture.diary.common.filter;
 
+import com.picture.diary.common.jwt.JwtEntity;
 import com.picture.diary.common.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
@@ -22,10 +24,18 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String accessToken = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
-        if(accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+        //TODO if depth 가 너무 많음 수정 필요
+        if(accessToken != null) {
+            JwtEntity validateResponseToken = jwtTokenProvider.getValidateResponseToken(accessToken);
+            //유효성 검사에 성공한 토큰이면
+            if(validateResponseToken.isValidate()) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                if(validateResponseToken.isNeedRefresh()) {
+                    jwtTokenProvider.setRefreshToken((HttpServletResponse) response, validateResponseToken);
+                }
+            }
         }
 
         chain.doFilter(request, response);
