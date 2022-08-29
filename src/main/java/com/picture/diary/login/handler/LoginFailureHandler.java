eb4ea5e035c414daed7d717a1d5b6d790b25ea13
hwkang <landman93@naver.com>
2022-08-29 20:06:19 +0900

    로그인 기능 구현중

---
+++ src/main/java/com/picture/diary/login/handler/LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package com.picture.diary.login.handler;
+
+import org.apache.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+        response.getWriter().print("{\"success\":false}");
+        response.getWriter().flush();
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
