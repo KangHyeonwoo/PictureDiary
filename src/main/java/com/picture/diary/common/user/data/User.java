cf93047bcb6b4965161d450d9d0aae2635e680dc
hwkang <landman93@naver.com>
2022-09-13 01:19:47 +0900

    로그인 기능 구현중

--- src/main/java/com/picture/diary/common/user/data/User.java
+++ src/main/java/com/picture/diary/common/user/data/User.java
@@ -2,14 +2,55 @@ package com.picture.diary.common.user.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
-public class User {
+public class User implements UserDetails {
 
     private String userId;
 
     private String username;
 
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority(role.name()));
+
+        return authorityList;
+    }
+
+    //비밀번호는 객체로 만들지 않는다.
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/main/java/com/picture/diary/common/user/service/UserService.java
+++ src/main/java/com/picture/diary/common/user/service/UserService.java
@@ -6,12 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Slf4j
+@Service
 public class UserService implements UserDetailsService {
-
-    private final LoginService loginService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
