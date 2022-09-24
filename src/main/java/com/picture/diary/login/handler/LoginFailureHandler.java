887bba4c1719901cb5be05062300c1460b08bebb
hwkang <landman93@naver.com>
2022-09-24 14:10:12 +0000

    로그인 기능 구현중

--- src/main/java/com/picture/diary/login/handler/LoginFailureHandler.java
+++ src/main/java/com/picture/diary/login/handler/LoginFailureHandler.java
@@ -1,5 +1,6 @@
 package com.picture.diary.login.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error("login fail");
         response.setStatus(HttpStatus.SC_UNAUTHORIZED);
         response.getWriter().print("{\"success\":false}");
         response.getWriter().flush();
--- src/main/java/com/picture/diary/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/picture/diary/login/handler/LoginSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.picture.diary.login.handler;
 
+import com.picture.diary.common.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -11,11 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtProvider jwtProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setStatus(HttpStatus.SC_OK);
+        //TODO 여기에 토큰 먹여야 하나?
 
         super.onAuthenticationSuccess(request, response, authentication);
     }
